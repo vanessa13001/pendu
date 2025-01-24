4e99404b4852c818dfe0a82632274756f8a305b5
joseph-mangeot <jolyne.mangeot@gmail.com>
2025-01-24 15:10:30 +0100

    fix: recover correct letters list in game_module settings
    wrong letters list was added leading to crash upon letter input

--- game_module/__settings__.py
+++ game_module/__settings__.py
@@ -1,31 +1,8 @@
 import string
 upper_letters = string.ascii_uppercase
+letters = string.ascii_letters
 cursor = {
-    "save" : "\033[s",
-    "load" : "\033[u",
-
-    "line_clear" : "\033[2K",
-    "light_line_clear" : "\033[0K",
-    "light_clear" : "\033[0J",
     "heavy_clear" : "\033[3J\033[1;0H\033[0J",
-
-    "bold_start" : "\033[1m",
-    "dim_start" : "\033[2m",
-    "bold_dim_end" : "\033[22m",
-
-    "underline_start" : "\033[4m",
-    "underline_end" : "\033[24m",
-
-    "calc_box_style_start" : "\033[0;1;38;5;129m",
-    "historic_data_style_start" : "\033[0;1;2;38;5;82m",
-    "results_data_style_start" : "\033[0;1;38;5;82m",
-
-    "instructions_style_start" : "\033[0;37m",
-    "result_message_style_start" : "\033[0;1;37m",
-    "error_message_style_start" : "\033[0;6;1;31m",
-    "happy_message_style_start" : "\033[0;6;1;38;5;201m",
-
-    "style_finish" : "\033[0m"
 }
 def cursor_line(y,x=0):
     coordinate = "\033[" + str(y) + ";" + str(x) + "H"
