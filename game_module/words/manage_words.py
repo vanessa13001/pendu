46145b03b40b7ad2b5519ed3680ecc030622233c
Adeline <adeline.patenne@hotmail.fr>
2025-01-24 14:54:31 +0100

    fix: add_word do not allow invalid input anymore

--- game_module/words/manage_words.py
+++ game_module/words/manage_words.py
@@ -3,24 +3,40 @@ import secrets
 
 def add_word(new_word, game_mode):
     new_word = str(new_word).strip().lower()
-    if game_mode == 0:
-        secret_words = open("game_module/words/words_easy.txt", "r", encoding="UTF-8")
-    else :
-        secret_words = open("game_module/words/words_hard.txt", "r", encoding="UTF-8")
-    secret_words_list = secret_words.readlines()
-    secret_words.close()
-    for index in range(len(secret_words_list)):
-        secret_words_list[index] = secret_words_list[index].replace("\n", "")
-    if new_word in secret_words_list:
-        return False
-    else:
+
+    special_caracter = ["'", "-", " "]
+
+    is_valid = True
+    for caracter in special_caracter:
+        if caracter in new_word:
+            get_index_caracter = new_word.index(caracter)
+            if new_word[(get_index_caracter)+1] in special_caracter:
+                is_valid = False
+                return "invalid"
+    
+    if is_valid:
         if game_mode == 0:
-            secret_words = open("game_module/words/words_easy.txt", "a", encoding="UTF-8")
-        else:
-            secret_words = open("game_module/words/words_hard.txt", "a", encoding="UTF-8")
-        secret_words.write("\n"+new_word)
+            secret_words = open("game_module/words/words_easy.txt", "r", encoding="UTF-8")
+        else :
+            secret_words = open("game_module/words/words_hard.txt", "r", encoding="UTF-8")
+        
+        secret_words_list = secret_words.readlines()
         secret_words.close()
-        return True
+
+        for index in range(len(secret_words_list)):
+            secret_words_list[index] = secret_words_list[index].replace("\n", "")
+        if new_word in secret_words_list:
+            return False
+        
+        else:
+            if game_mode == 0:
+                secret_words = open("game_module/words/words_easy.txt", "a", encoding="UTF-8")
+            else:
+                secret_words = open("game_module/words/words_hard.txt", "a", encoding="UTF-8")
+
+            secret_words.write("\n"+new_word)
+            secret_words.close()
+            return True    
 
 def get_guess_word(game_mode):
     if game_mode == 0:
