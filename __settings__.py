5261c6c9082e16a84515e129f46fed8fdee30692
Adeline <adeline.patenne@hotmail.fr>
2025-01-24 11:28:29 +0100

    fix: add dictionary for hovered button

--- __settings__.py
+++ __settings__.py
@@ -1,3 +1,13 @@
 import string
 upper_letters = string.ascii_uppercase
 letters = string.ascii_letters
+
+hovered = {
+    "start_button" : False,
+    "score_button" : False,
+    "quit_button" : False,
+    "reset_button" : False,
+    "back_button" : False,
+    "left" : False,
+    "right" : False
+}
